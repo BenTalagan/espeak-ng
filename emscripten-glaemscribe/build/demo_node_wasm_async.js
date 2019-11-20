ebe3e64a7e2396f12efcc65b82db298b163b4655
BenTalagan <ben_talagan@github.nospam.com>
2019-11-20 21:19:54 +0100

    Fixing demos

--- emscripten-glaemscribe/build/demo_node_wasm_async.js
+++ emscripten-glaemscribe/build/demo_node_wasm_async.js
@@ -11,6 +11,9 @@ Path     = require("path")
 Util     = require("util")
 print    = console.log;
 
+// The two following lines seems to be required now
+global.__dirname = __dirname;
+global.require = require;
 
 // Use the following trick to load the javascript that we would use normally in a web browser
 function include(path) { var code = Fs.readFileSync(path, 'utf-8'); Vm.runInThisContext(code, path); }
